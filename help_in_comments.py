01590fa9451f451b43b272d01cb9d1b7d172d60b
erlaveri <vslavkarpov@gmail.com>
2014-09-01 14:20:29 +0300

    s:

---
+++ help_in_comments.py
@@ -0,0 +1,4 @@
+ss = 'simple_ -str:.ing'
+dd = ss.translate(None, '-: .')
+print dd
+'simple_string'
--- templatetags/erlap_tags.py
+++ templatetags/erlap_tags.py
@@ -1,5 +1,6 @@
 from django.template import Node
 from django import template
+import re
 
 try:
     import ipdb as pdb
@@ -18,3 +19,33 @@ class PdbNode(Node):
 @register.tag
 def pdb_debug(parser, token):
     return PdbNode()
+
+
+@register.inclusion_tag('erlap/pagination.html', takes_context=True)
+def erl_pagination(context, page):
+    paginator = page.paginator
+    all_pages = paginator.num_pages
+    cur = page.number
+    pages = [1, ]
+
+    if all_pages > 1:
+        if cur - 1 > 3:
+            pages.append(None)
+            pages += range(cur - 2, cur + 1)
+
+        else:
+            pages += range(2, cur + 1)
+        # ###########################################
+        if all_pages - cur > 3:
+            pages += range(cur + 1, cur + 3)
+            pages.append(None)
+            pages.append(all_pages)
+        else:
+            pages += range(cur + 1, all_pages + 1)
+
+    req = context['request']
+    query_vars = re.sub(r'page=\d+&?', '', req.GET.urlencode())
+    query_vars = '&' + query_vars if query_vars else query_vars
+    context['query_vars'] = query_vars
+
+    return {'pages': pages, 'query_vars': query_vars, 'cur': cur}

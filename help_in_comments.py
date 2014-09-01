dc3c12d556504d311c5765d584e9d7bb599ea78d
erlaveri <vslavkarpov@gmail.com>
2014-09-02 01:06:45 +0300

    s:

--- help_in_comments.py
+++ help_in_comments.py
@@ -1,4 +1,10 @@
+import random
+
 ss = 'simple_ -str:.ing'
 dd = ss.translate(None, '-: .')
 print dd
 'simple_string'
+
+dd = random.sample('qwerty', 6)
+print(''.join(dd))
+'yqertw'
--- py_ytils.py
+++ py_ytils.py
@@ -1,5 +1,22 @@
+import os
+import random
+import string
+import inspect
+import uuid
+
+
 def lineno():
     """Returns the current line number in our program."""
-    import inspect
-
     return inspect.currentframe().f_back.f_lineno
+
+
+def token_nice(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+
+def token_uuid(ver=4):
+    return uuid.uuid4().hex if ver == 4 else uuid.uuid1().hex
+
+
+def token_os(n=32):
+    return os.urandom(n)

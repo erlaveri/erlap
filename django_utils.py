4d171ad51bf8eba5d704da83341bb621d3e304f5
erlaveri <erlaveri@gmail.com>
2014-09-12 20:51:59 +0300

    s:

--- django_utils.py
+++ django_utils.py
@@ -1,2 +1,19 @@
+from urlparse import urlparse
+from django.http import HttpResponseRedirect
+
+
 def get_from_choices(choices, f_name):
     return [i for i in choices if i[1] == 'xuxa'][0][0]
+
+
+
+# def redirect_for_login(request):
+#     referer = request.META.get('HTTP_REFERER', None)
+#     to = '/'
+#     if referer:
+#         parsed_uri = urlparse(referer)
+#         referer_host = '{uri.netloc}'.format(uri=parsed_uri)
+#         if referer_host == request.META.get('HTTP_HOST', None):
+#             to = referer
+#
+#     return HttpResponseRedirect(to + '?next=' + request.path_info)

8c8c39f239fb395a96853528d7a2e291eb74cf53
erlaveri <erlaveri@gmail.com>
2014-11-07 00:51:46 +0200

    s: fix

--- static/js/erl-utils.js
+++ static/js/erl-utils.js
@@ -92,8 +92,7 @@ erljs.shuffle = {
         for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
     }
-}
-;
+};
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 erljs.getOriginalOfImg = function (img_element) {
     var native = img_element.get(0);
@@ -162,13 +161,45 @@ erljs.range = function (start, stop, step) {
     return result;
 };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+erljs.style = {
+//http://stackoverflow.com/questions/754607/can-jquery-get-all-css-styles-associated-with-an-element
+//Pass a jQuery object into css() and it will return an object, which you can then plug back into jQuery's $().css(), ex:
+//
+//var style = css($("#elementToGetAllCSS"));
+//$("#elementToPutStyleInto").css(style);
 
+    css: function (a) {
+        var sheets = document.styleSheets, o = {};
+        for (var i in sheets) {
+            var rules = sheets[i].rules || sheets[i].cssRules;
+            for (var r in rules) {
+                if (a.is(rules[r].selectorText)) {
+                    o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
+                }
+            }
+        }
+        return o;
+    },
 
-
-
-
-
-
-
+    css2json: function (css) {
+        var s = {};
+        if (!css) return s;
+        if (css instanceof CSSStyleDeclaration) {
+            for (var i in css) {
+                if ((css[i]).toLowerCase) {
+                    s[(css[i]).toLowerCase()] = (css[css[i]]);
+                }
+            }
+        } else if (typeof css == "string") {
+            css = css.split("; ");
+            for (var i in css) {
+                var l = css[i].split(": ");
+                s[l[0].toLowerCase()] = (l[1]);
+            }
+        }
+        return s;
+    }
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 self.erljs = erljs;

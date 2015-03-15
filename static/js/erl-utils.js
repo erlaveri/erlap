718172468acbe83ed938497557ed44e4cb95f8af
erlaveri <erlaveri@gmail.com>
2015-03-15 10:13:11 +0200

    Update erl-utils.js

--- static/js/erl-utils.js
+++ static/js/erl-utils.js
@@ -40,6 +40,19 @@ erljs.QueryStringToJSON = function (query) {
     });
     return JSON.parse(JSON.stringify(result));
 };
+
+erljs.serialize = function(obj, prefix) {
+  var str = [];
+  for(var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+      str.push(typeof v == "object" ?
+        serialize(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 erljs.shuffle = {
     fisher_yates: function (array) {

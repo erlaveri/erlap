a3251c05f0dac1f0d0076ab336b635ccfb5ac7e1
erlaveri <erlaveri@gmail.com>
2016-09-14 21:53:27 +0300

    pagination tag

--- templatetags/erlap_tags.py
+++ templatetags/erlap_tags.py
@@ -56,3 +56,40 @@ def erl_pagination(context, page):
     context['query_vars'] = query_vars
 
     return {'pages': pages, 'query_vars': query_vars, 'cur': cur}
+
+
+@register.assignment_tag(takes_context=True)
+def au_pagination(context):
+    page_obj = context.get('page_obj')
+    if page_obj:
+        paginator = page_obj.paginator
+        all_pages = paginator.num_pages
+        cur = page_obj.number
+        pages = [1, ]
+
+        if all_pages > 1:
+            if cur - 1 > 3:
+                pages.append(None)
+                pages += range(cur - 2, cur + 1)
+
+            else:
+                pages += range(2, cur + 1)
+            # ###########################################
+            if all_pages - cur > 3:
+                pages += range(cur + 1, cur + 3)
+                pages.append(None)
+                pages.append(all_pages)
+            else:
+                pages += range(cur + 1, all_pages + 1)
+
+        req = context['request']
+
+        query_vars = req.GET.copy()
+        if 'page' in query_vars:
+            query_vars.pop('page')
+        query_vars = query_vars.urlencode()
+
+        query_vars = '&' + query_vars if query_vars else query_vars
+        context.update({'au_p_pages': pages, 'au_p_query_vars': query_vars, 'au_p_cur': cur})
+
+    return context
